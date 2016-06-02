c3c4a5b015e3f557c8ebdff7446a32384606b45b
Kevin E Meraz <kevinemeraz@gmail.com>
2016-06-02 15:33:58 -0700

    Modify to include ES6 linting

--- client/app.jsx
+++ client/app.jsx
@@ -1,17 +1,17 @@
- 'use strict';
-
 import React from 'react';
-import { render } from 'react-dom';
 
-var App = React.createClass({
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
 
   render() {
     return <h1>Hello, World</h1>;
-  },
+  }
 
-});
+}
 
-render (
-  <App />,
-  document.getElementById('app')
-);
+export default App;
